ee159228311fdcc2f073fbbc742cf24cb23245df
Andie Lin <andie@Andies-MBP.modem>
2023-12-09 20:56:51 +1100

    speed

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,13 +1,13 @@
 import {motion} from "framer-motion"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const LayoutCard= () =>{
 
     return(
-        <>
+        <SpeedInsights>
         <motion.div>
             
         </motion.div>
-        </>
+        </SpeedInsights>
     )
 
 }
